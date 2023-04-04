42a61c0818a9ec5e7715beb9c498eb0be693c189
Brett Wiseman <bwisema3@nd.edu>
2023-04-04 12:08:51 -0400

    get this ball rollingggggggggggg

--- GREGCLient.py
+++ GREGCLient.py
@@ -1,1 +1,49 @@
 # GREG Client
+import zmq
+import chess
+import sys
+
+context = zmq.Context()
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://localhost:5555")
+
+def main():
+
+    print("Welcome to the GREG chess application! (q to quit)")
+    board = chess.Board()
+        
+
+    while(True):
+        print(board)
+        if board.is_checkmate():
+            print(f"game over! {board.outcome().result()}")
+            exit(0)
+        move = input("Make your move (uci): \n")
+        if move == "q":
+            print("Ending Game")
+            exit(0)
+        
+        try:
+            move = chess.Move.from_uci(move)
+        except:
+            print("please make a valid move")
+            continue
+
+        if move not in board.legal_moves:
+            print("please make a valid move")
+            continue
+
+        board.push(move)
+
+        # get next move from server
+        b = board.fen()
+        socket.send(bytes(b, "utf-8"))
+
+        move = socket.recv().decode("utf-8")
+        move = chess.Move.from_uci(move)
+        board.push(move)
+        
+
+
+if __name__ == "__main__":
+    main()
--- GREGServer.py
+++ GREGServer.py
@@ -1,1 +1,26 @@
-# Greg Server
+#Greg Server
+
+import time
+import zmq
+import chess
+
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:5555")
+
+while True:
+    #  Wait for next request from client
+    message = socket.recv()
+    print(f"{message}")
+
+    b = message.decode("utf-8")
+    board = chess.Board(fen=b)
+    print(board)
+    #  Do some 'work'
+    while True:
+        move = input("Make your move (uci): \n")
+        if chess.Move.from_uci(move) in board.legal_moves:
+            break
+        
+    #  Send reply back to client
+    socket.send(bytes(move,"utf-8"))
